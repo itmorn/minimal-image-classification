d6fc2456cbda5d2630411fb846776850d6b12f7b
itmorn <12567148@qq.com>
2022-07-20 23:48:16 +0800

    删除无用文件

---
+++ models/model_alexnet.py
@@ -0,0 +1,130 @@
+"""
+@Auth: itmorn
+@Date: 2022/7/19-16:08
+@Email: 12567148@qq.com
+"""
+
+
+def inference_office_model():
+    from torchvision.io.image import read_image
+    from torchvision.models.alexnet import alexnet, AlexNet_Weights
+    img = read_image("../images/balloon.jpg")
+
+    weights = AlexNet_Weights.DEFAULT
+    model = alexnet(weights=weights)
+    model.eval()
+
+    preprocess = weights.transforms()
+
+    batch = preprocess(img).unsqueeze(0)
+
+    prediction = model(batch)[0].softmax(0)
+    idx, score = prediction.argmax(), prediction.max()
+
+    categories = weights.meta["categories"][idx]
+    print(categories, float(score))
+
+
+def vis_office_model():
+    import torch
+    from torchvision.io.image import read_image
+    from torchvision.models.alexnet import alexnet, AlexNet_Weights
+    import onnx.version_converter
+    img = read_image("../images/balloon.jpg")
+
+    weights = AlexNet_Weights.DEFAULT
+    model = alexnet(weights=weights)
+    model.eval()
+
+    torch.save(model, 'alexnet.pth')
+
+    preprocess = weights.transforms()
+
+    batch = preprocess(img).unsqueeze(0)
+
+    torch.onnx.export(
+        model,
+        batch,
+        'alexnet.onnx',
+        export_params=True,
+        opset_version=16,
+    )
+
+    # 增加维度信息
+    model_file = 'alexnet.onnx'
+    onnx_model = onnx.load(model_file)
+    onnx.save(onnx.shape_inference.infer_shapes(onnx_model), model_file)
+    print("done")
+
+
+def eval_office_model():
+    import torch
+    import torch.backends.cudnn
+    from torch import nn
+    import torchvision
+    import torch.utils.data
+    from torchvision.models.alexnet import alexnet, AlexNet_Weights
+    import utils
+    from tqdm import tqdm
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    valdir = '/data01/zhaoyichen/data/ImageNet1k/val'
+
+    weights = AlexNet_Weights.DEFAULT
+    preprocessing = weights.transforms()
+
+    dataset_test = torchvision.datasets.ImageFolder(
+        valdir,
+        preprocessing,
+    )
+    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=32, sampler=test_sampler, num_workers=1, pin_memory=True
+    )
+
+    model = alexnet(weights=weights)
+    model.to(device)
+    model.eval()
+
+    criterion = nn.CrossEntropyLoss(label_smoothing=0)
+
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    acc1_all = 0
+    acc5_all = 0
+    loss_all = 0
+    num = 0
+    with torch.inference_mode():
+        for image, target in tqdm(data_loader_test):
+            image = image.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
+            output = model(image)
+            loss = criterion(output, target)
+
+            acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+            # FIXME need to take into account that the datasets
+            # could have been padded in distributed setup
+            batch_size = image.shape[0]
+
+            acc1_all += acc1.item()*batch_size
+            acc5_all += acc5.item()*batch_size
+            loss_all += loss.item()*batch_size
+            num += batch_size
+
+            print(f"acc1:{acc1_all/num}\tacc5:{acc5_all/num}\tloss:{loss/num}\t")
+
+
+
+if __name__ == '__main__':
+    # 使用官方预训练模型推理
+    # inference_office_model()
+
+    # 官方模型可视化
+    # vis_office_model()
+
+    # 在ImageNet的val上评估官方预训练模型的效果
+    eval_office_model()
+
+    # 训练模型
+    # train_model()
---
+++ utils.py
@@ -0,0 +1,25 @@
+"""
+@Auth: itmorn
+@Date: 2022/7/20-20:59
+@Email: 12567148@qq.com
+"""
+import torch
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.inference_mode():
+        maxk = max(topk)
+        batch_size = target.size(0)
+        if target.ndim == 2:
+            target = target.max(dim=1)[1]
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target[None])
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].flatten().sum(dtype=torch.float32)
+            res.append(correct_k * (100.0 / batch_size))
+        return res
